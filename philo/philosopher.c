5a52e4ff9104227ee0902d05fbf6ade03c0431d4
Tom <tjans@student.codam.nl>
2021-09-28 00:05:10 +0200

    Better output

--- philo/philosopher.c
+++ philo/philosopher.c
@@ -1,37 +1,41 @@
 #include "philosophers.h"
 
+static int	get_timestamp(t_philosopher *data)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (ms_between_timestamps(data->start_time, &tv));
+}
+
 static void	p_eat(t_philosopher *data)
 {
 	pthread_mutex_lock(data->cs_left);
-	printf("Philosopher %d took left fork\n", data->philosopher_number);
+	printf("[%d] %d has taken a fork\n", get_timestamp(data),
+		data->philosopher_number);
 	pthread_mutex_lock(data->cs_right);
-	printf("Philosopher %d took right fork\n", data->philosopher_number);
-	printf("Philosopher %d is eating\n", data->philosopher_number);
+	printf("[%d] %d has taken a fork\n", get_timestamp(data),
+		data->philosopher_number);
+	printf("[%d] %d is eating\n", get_timestamp(data),
+		data->philosopher_number);
 	gettimeofday(&data->s_last_feeding, NULL);
 	usleep(1000 * data->args->time_to_eat);
-	printf("Philosopher %d finished eating\n", data->philosopher_number);
 	pthread_mutex_unlock(data->cs_left);
 	pthread_mutex_unlock(data->cs_right);
 }
 
 void	*philosopher(t_philosopher *data)
 {
-	struct timeval	s_cur;
-	int				delta;
-
-	printf("Philosopher %d spawned\n", data->philosopher_number);
 	while (1)
 	{
 		p_eat(data);
-		printf("Philosopher %d sleeps.\n", data->philosopher_number);
+		printf("[%d] %d is sleeping\n", get_timestamp(data),
+			data->philosopher_number);
 		usleep(data->args->time_to_sleep * 1000);
-		gettimeofday(&s_cur, NULL);
-		delta = ms_between_timestamps(&data->s_last_feeding, &s_cur);
-		printf("Philosopher %d woke up! Took %dms\n", data->philosopher_number,
-			delta);
-		if (delta > data->args->time_to_die)
-			break ;
+		printf("[%d] %d is thinking\n", get_timestamp(data),
+			data->philosopher_number);
 	}
-	printf("Philosopher %d died!\n", data->philosopher_number);
+	printf("[%d] %d died\n", get_timestamp(data),
+		data->philosopher_number);
 	return (NULL);
 }
