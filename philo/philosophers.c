60c9035a034dd954fc310933b87e354d67418e54
Tom <tjans@student.codam.nl>
2021-10-05 06:27:21 +0200

    fix memleaks

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -14,13 +14,29 @@ static void	join_threads(t_philosophers *philosophers)
 	}
 }
 
+static void	cleanup(t_philosophers *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->args.num_of_philosophers)
+	{
+		free((*(philo->chopsticks + i))->mutex);
+		free(*(philo->chopsticks + i));
+		free(*(philo->entities + i));
+		i++;
+	}
+	free(philo->chopsticks);
+	free(philo->entities);
+	free(philo->state_lock);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philosophers	philosophers;
 
 	memset(&philosophers, 0, sizeof(t_philosophers));
 	philosophers.state_lock = malloc(sizeof(pthread_mutex_t));
-	pthread_mutex_init(philosophers.state_lock, NULL);
 	gettimeofday(&philosophers.start_time, NULL);
 	if (get_args(&philosophers.args, argc, argv))
 	{
@@ -29,8 +45,10 @@ int	main(int argc, char **argv)
 			"[number_of_times_each_philosopher_must_eat]");
 		return (1);
 	}
+	pthread_mutex_init(philosophers.state_lock, NULL);
 	spawn_chopsticks(&philosophers);
 	spawn_philosophers(&philosophers);
 	join_threads(&philosophers);
+	cleanup(&philosophers);
 	return (0);
 }
