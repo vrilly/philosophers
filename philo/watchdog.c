22c753d1419b6280ef1f5575b21c13be5501d98a
Tom Jans <tjans@fluffderg.me>
2021-10-14 16:30:33 +0200

    fix race condition

--- philo/watchdog.c
+++ philo/watchdog.c
@@ -8,10 +8,10 @@ static int	check_philo(t_philosopher *data, struct timeval cur)
 	if (d > data->globals->args.time_to_die
 		&& data->times_eaten != data->globals->args.times_to_eat)
 	{
-		pthread_mutex_lock(&data->globals->state_lock);
 		data->globals->dead = data->philosopher_number;
 		pthread_mutex_unlock(&data->globals->state_lock);
 		printer(data, DIED, NULL);
+		pthread_mutex_lock(&data->globals->state_lock);
 		return (1);
 	}
 	return (0);
@@ -22,8 +22,9 @@ void	*watchdog(t_philosophers *philos)
 	int				i;
 	struct timeval	cur;
 
-	while (philos->globals.dead == 0 && philos->globals.done != philos->globals.args.num_of_philosophers)
+	while (1)
 	{
+		pthread_mutex_lock(&philos->globals.state_lock);
 		i = 0;
 		gettimeofday(&cur, NULL);
 		while (i < philos->globals.args.num_of_philosophers)
@@ -31,8 +32,12 @@ void	*watchdog(t_philosophers *philos)
 			if (check_philo(philos->entities[i], cur))
 				break ;
 			i++;
-		}		
+		}
+		if (philos->globals.dead || philos->globals.done == philos->globals.args.num_of_philosophers)
+			break ;
+		pthread_mutex_unlock(&philos->globals.state_lock);
 		usleep(1000);
 	}
+	pthread_mutex_unlock(&philos->globals.state_lock);
 	return (NULL);
 }
